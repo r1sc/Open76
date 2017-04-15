512e7723e6a0737ce9c685f7e35623dd3c39726b
r1sc <sandberg.erik@gmail.com>
2017-04-15 23:11:22 +0200

    Started writing a VDF parser

--- Assets/Fileparsers/VdfParser.cs
+++ Assets/Fileparsers/VdfParser.cs
@@ -7,7 +7,26 @@ namespace Assets.Fileparsers
 {
     public class Vdf
     {
+        public float Unk70f;
+        public float Unk35f;
+        public float Unk30f;
+        public float Unk25f;
+        public byte Unk255b;
+        public byte Unk255b2;
+        public byte Unk127b;
+        public byte Unk127b2;
+        public float Unk1320f;
+        public float Unk1f;
+        public byte Unk63b;
+        public byte Unk82b;
+        public byte Unk73b;
+        public byte Unk157b;
+        public byte Unk57b;
         public string Name { get; set; }
+        public uint Unk0 { get; set; }
+        public uint Unk1 { get; set; }
+        public uint Unk2 { get; set; }
+        public uint Unk4 { get; set; }
     }
 
     public class VdfParser
@@ -20,6 +39,27 @@ namespace Assets.Fileparsers
 
                 var vdf = new Vdf();
                 vdf.Name = br.ReadCString(16);
+                vdf.Unk0 = br.ReadUInt32();
+                vdf.Unk1 = br.ReadUInt32();
+                vdf.Unk2 = br.ReadUInt32();
+                vdf.Unk70f = br.ReadSingle();
+                vdf.Unk35f = br.ReadSingle();
+                vdf.Unk30f = br.ReadSingle();
+                vdf.Unk25f = br.ReadSingle();
+                vdf.Unk255b = br.ReadByte();
+                vdf.Unk255b2 = br.ReadByte();
+                vdf.Unk127b = br.ReadByte();
+                vdf.Unk127b2 = br.ReadByte();
+                vdf.Unk1320f = br.ReadSingle();
+                vdf.Unk1f = br.ReadSingle();
+                vdf.Unk63b = br.ReadByte();
+                vdf.Unk82b = br.ReadByte();
+                vdf.Unk73b = br.ReadByte();
+                vdf.Unk157b = br.ReadByte();
+                vdf.Unk57b = br.ReadByte();
+
+
+                vdf.Unk4 = br.ReadUInt32();
             }
         }
     }
