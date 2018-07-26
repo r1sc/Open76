18e7eb551956bbf6ee11715960399f066d1a820d
Johan Rensenbrink <admin@renscreations.com>
2018-07-26 22:44:44 +1000

    Added small native library and Unity scene for playing the SMK cutscene files.

---
+++ libsmacker/UnityInterface.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "smacker.h"
+
+struct Color32
+{
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+	unsigned char A;
+};
+
+const int MaxAudioTracks = 7;
+
+struct SmkAudioData
+{
+	unsigned char TrackMask;
+	unsigned char AudioChannels[MaxAudioTracks];
+	unsigned long AudioRate[MaxAudioTracks];
+	unsigned char BitDepth[MaxAudioTracks];
+	unsigned long AudioDataSize[MaxAudioTracks];
+	unsigned char* AudioData[MaxAudioTracks];
+};
+
+extern "C"
+{
+	// Open an SMK file from disk - return video width, height, frame count and microseconds between frames.
+	__declspec(dllexport) void* OpenFile(const char* filePath, int& width, int& height, int& frameCount, float& microSecondsPerFrame);
+
+	// Clean up SMK file.
+	__declspec(dllexport) void DisposeFile(smk smkFileHandle);
+
+	// Fill Color32 buffer with current frame's data.
+	__declspec(dllexport) void GetFrameData(smk smkFileHandle, Color32* buffer, unsigned int bufferSize);
+
+	// Attempt to advance to the next frame, returns false if there are no more frames.
+	__declspec(dllexport) bool AdvanceFrame(smk smkFileHandle);
+	
+	// Load raw audio data.
+	__declspec(dllexport) void* GetAudioData(smk smkFileHandle);
+	
+	// Clean up audio data.
+	__declspec(dllexport) void DisposeAudioData(SmkAudioData* audioData);
+}
