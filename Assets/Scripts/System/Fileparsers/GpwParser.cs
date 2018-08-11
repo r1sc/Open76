280658e6aeaab9ebabe677c74e09de4dc252d218
Johan Rensenbrink <admin@renscreations.com>
2018-08-11 16:27:02 +1000

    Rebased GPW parser - use new FastBinaryReader class.

---
+++ Assets/Scripts/System/Fileparsers/GpwParser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.System;
+using Assets.System;
+using UnityEngine;
+
+namespace Assets.Fileparsers
+{
+    public class Gpw
+    {
+        public AudioClip Clip { get; set; }
+        public short AudioRange { get; set; }
+    }
+
+    public class GpwParser
+    {
+        private static readonly Dictionary<string, Gpw> GpwCache = new Dictionary<string, Gpw>();
+
+        public static Gpw ParseGpw(string fileName)
+        {
+            Gpw gpw;
+            if (GpwCache.TryGetValue(fileName, out gpw))
+            {
+                return gpw;
+            }
+
+            using (FastBinaryReader br = VirtualFilesystem.Instance.GetFileStream(fileName))
+            {
+                gpw = new Gpw();
+                string header = br.ReadCString(4); // Always GAS0
+                gpw.AudioRange = (short)br.ReadUInt16();
+                ushort unk2 = br.ReadUInt16();
+                int unk3 = br.ReadInt32();
+                int unk4 = br.ReadInt32();
+                int unk5 = br.ReadInt32();
+                int unk6 = br.ReadInt32();
+                int unk7 = br.ReadInt32();
+                br.Position += 4; // Skip RIFF header
+                int waveFileSize = br.ReadInt32(); // Read total size of audio data from RIFF header
+                br.Position -= 8; // Go back to RIFF header
+
+                byte[] audioData = br.ReadBytes(waveFileSize);
+                gpw.Clip = WavUtility.ToAudioClip(audioData, 0, fileName);
+
+                float unk8 = br.ReadSingle();
+                float unk9 = br.ReadSingle();
+
+                GpwCache.Add(fileName, gpw);
+                return gpw;
+            }
+        }
+    }
+}
