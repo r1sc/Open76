862dc684b5ebef07562db0083db0fbe40a07a779
Johan Rensenbrink <admin@renscreations.com>
2018-09-02 15:10:45 +1000

    Added a bare XDF parser (they appear to be sprites/effects)

---
+++ Assets/Scripts/System/Fileparsers/XdfParser.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Fileparsers
+{
+    public class Xdf
+    {
+    }
+
+    class XdfParser
+    {
+        public static Xdf ParseXdf(string filename)
+        {
+            using (var br = new Bwd2Reader(filename))
+            {
+                var xdf = new Xdf();
+
+                br.FindNext("WDFC");
+                
+
+                return xdf;
+            }
+        }
+    }
+}
