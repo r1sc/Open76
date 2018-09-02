e9b43a663f7baa5eae75139f6d46523ed7fb4810
Johan Rensenbrink <admin@renscreations.com>
2018-09-02 15:11:12 +1000

    Added a projectile class and a prefab to be used for weapon projectiles.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+        public float Velocity { get; set; }
+        public float Damage { get; set; }
+        public Transform Owner { get; set; }
+
+        private const float MaxLifeTime = 10.0f;
+
+        private float _lifeTime;
+        
+        private void Update()
+        {
+            float dt = Time.deltaTime;
+
+            if (Velocity > 0.0f)
+            {
+                transform.Translate(Vector3.forward * Velocity * dt, Space.Self);
+            }
+
+            _lifeTime += dt;
+            if (_lifeTime > MaxLifeTime)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Transform parent = other.transform;
+            while (parent.parent != null)
+            {
+                parent = parent.parent;
+            }
+
+            if (parent == Owner)
+            {
+                return;
+            }
+
+            // TODO: Deal damage.
+            // TODO: Spawn impact sprite.
+            Destroy(gameObject);
+        }
+    }
+}
